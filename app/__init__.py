a60737fc4de258b0f3b8d63c37d30e9b37b442f1
Kandrianaha <k.andria@icloud.com>
2026-03-16 19:13:17 +1100

    updating the JWTManager, secret key, init-app, auth namespace

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,14 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as review_ns
 from app.api.v1.amenities import api as amenities_ns
+from app.api.v1.auth import api as auth_ns
+from flask_jwt_extended import JWTManager
+jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
+    app.config['JWT_SECRET_KEY']='super-secret-key'
+    jwt.init_app(app)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
 
     # Placeholder for API namespaces (endpoints will be added later)
@@ -15,5 +20,6 @@ def create_app():
     api.add_namespace(review_ns, path='/api/v1/review')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     # Additional namespaces for places, reviews, and amenities will be added later
+    api.add_namespace(auth_ns, path='/api/v1/auth')
 
     return app
