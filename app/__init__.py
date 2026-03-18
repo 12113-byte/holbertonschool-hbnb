eb19864effc2065ec8fcdbbda16a1e8341e6101a
pavithpr89 <pavithpr89@gmail.com>
2026-03-18 21:25:20 +0000

    Added the sql repo updated the services/facade and added stuff to _init_ file

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,9 @@ from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as review_ns
 from app.api.v1.amenities import api as amenities_ns
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
 def create_app():
     app = Flask(__name__)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
@@ -15,5 +18,22 @@ def create_app():
     api.add_namespace(review_ns, path='/api/v1/review')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     # Additional namespaces for places, reviews, and amenities will be added later
+
+    return app
+
+#check this
+
+# Create global instances
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+
+
+def create_app(config_class):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    # Initialize extensions
+    db.init_app(app)
+    bcrypt.init_app(app)
 
     return app
