2227ba88c8fd4557792f110fa4fc9838a73f9901
Kandrianaha <k.andria@icloud.com>
2026-03-21 14:29:32 +1100

    update schema

--- part3/schema.sql
+++ part3/schema.sql
@@ -47,7 +47,7 @@ CREATE TABLE IF NOT EXISTS Place_Amenity (
 -- inserting admin user 
 INSERT INTO User (id, first_name, last_name, email, password, is_admin)
 VALUES (
-    '36c9050e-ddd3-4c3b-9731-9f487208bbc1'
+    '36c9050e-ddd3-4c3b-9731-9f487208bbc1',
     'Admin',
     'HBnB',
     'admin@hbnb.io',
