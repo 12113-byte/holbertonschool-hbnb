62013a20aa358fd6190a46e2e8cc8b1ad8cde8ed
Joleen <12113@holbertonstudents.com>
2026-03-30 19:25:34 +1100

    added TestingConfig and ProductionConfig classes

--- config.py
+++ config.py
@@ -7,7 +7,15 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
 
+class TestingConfig(Config):
+    DEBUG = True
+
+class ProductionConfig(Config):
+    pass
+
 config = {
     'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
     'default': DevelopmentConfig
 }
