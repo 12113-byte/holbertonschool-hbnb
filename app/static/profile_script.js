9d33f7e192e06bbecea6ff07fc6e89f0364ec616
Joleen <12113@holbertonstudents.com>
2026-04-09 17:22:48 +1000

    added user profile

---
+++ app/static/profile_script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    checkAuthentication();
+  });
+
+  function getCookie(name) {
+    const cookies = document.cookie.split('; ');
+    const token = cookies.find(cookie => cookie.startsWith(name + '='));
+    if (!token) {
+      window.location.href = 'login.html'; // if not signed in, redirect to login page
+    }
+    const extracted_name = token.split('=');
+    return extracted_name[1];
+}
